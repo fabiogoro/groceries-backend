399bbada1668e355b4f50f59c336249a3819b4a9
Fabio Gorodscy <fabiogo@outlook.com>
2023-09-04 11:34:12 -0300

    Added search query param

--- DAL/DAL.js
+++ DAL/DAL.js
@@ -10,8 +10,8 @@ class DAL{
     return (await this.connection.execute(`SELECT * FROM user`))[0]
   }
 
-  async getGroceries({sort_by, categories}){
-    return (await this.connection.execute(`SELECT *, (select path from grocery_picture where grocery=grocery.id limit 1) thumbnail FROM grocery ${categories?`where category in (${categories})`:''} ORDER BY ${sort_by.replace('.',' ')}`))[0]
+  async getGroceries({sort_by, categories, q, page}){
+    return (await this.connection.execute(`SELECT *, (select path from grocery_picture where grocery=grocery.id limit 1) thumbnail FROM grocery where title like '%${q}%' ${categories?` and category in (${categories})`:''} ORDER BY ${sort_by.replace('.',' ')} LIMIT ${(page-1)*20},${page*20}`))[0]
   }
 
   async getGrocery(id){
